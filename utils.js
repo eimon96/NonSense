89555e6fef43416d503c0685ed0b85afa89f6280
eimon <>
2025-04-22 23:37:24 +0300

    Map analog stick input to mouse movement

--- utils.js
+++ utils.js
@@ -25,11 +25,15 @@ export const getSenseDevices = () => {
 };
 
 export const readRawInput = (device) => {
+  let lastData = null;
+  const interval = 24;
   device.getFeatureReport(0x05, 128);
+
   const prevState = {};
   device.on("data", (data) => {
     const inputs = mapInputs(data);
     Object.entries(inputs).map(([key, value]) => {
+      lastData = data;
       const currentState = !!value;
       if (!!currentState && !prevState[key]) {
         inputHandler(key);
@@ -37,14 +41,41 @@ export const readRawInput = (device) => {
       prevState[key] = currentState;
     });
   });
+
+  setInterval(() => {
+    if (lastData) {
+      moveMouseWithStick(lastData);
+    }
+  }, interval);
 };
 
 export const inputHandler = (key) => {
   switch (key) {
+    case BUTTONS.SQUARE:
     case BUTTONS.CROSS:
       robot.mouseClick();
       break;
+    case BUTTONS.L2:
+    case BUTTONS.R2:
+      robot.mouseClick("right");
+      break;
     default:
       break;
+  }
+};
+
+export const normalizeStick = (value) => {
+  return (value - 128) / 128;
+};
+
+export const moveMouseWithStick = (data) => {
+  const xInput = normalizeStick(data[2]);
+  const yInput = normalizeStick(data[3]);
+  const deadzone = 0.15;
+  const speed = 17;
+
+  if (Math.abs(xInput) > deadzone || Math.abs(yInput) > deadzone) {
+    const { x, y } = robot.getMousePos();
+    robot.moveMouse(x + xInput * speed, y + yInput * speed);
   }
 };
